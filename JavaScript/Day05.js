a6f3b15790de0158eeab9d7251c6421e92b0a762
Charles Willcockson <willcockson.charles@gmail.com>
2022-05-26 13:17:35 -0500

    Day5

---
+++ JavaScript/Day05.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function printTable(n){
+    let i;
+    let temp;
+    for(i=1; i<=10, i++){
+        temp = n * i;
+        console.log(n + ' x ' + i + ' = ' + temp)
+    }
+}
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    printTable(n);
+}
