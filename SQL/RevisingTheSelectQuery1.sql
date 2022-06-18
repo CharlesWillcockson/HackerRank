2325fdc54b8747e3ac21da3f486a2b948bd1e331
Charles Willcockson <willcockson.charles@gmail.com>
2022-06-18 13:51:07 -0500

    finished 15th sql exercise from hackerrank

--- SQL/RevisingTheSelectQuery1.sql
+++ SQL/RevisingTheSelectQuery1.sql
@@ -75,3 +75,9 @@ WHERE NOT (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' 
 
 SELECT distinct city from station
 WHERE NOT (city LIKE '%A' OR city LIKE '%E' OR city LIKE '%I' OR city LIKE '%O' OR city LIKE '%U');
+
+# Exercise 15
+
+SELECT distinct city from station
+WHERE NOT (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%') OR not (city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' OR city LIKE '%o' OR city LIKE '%u');
+
